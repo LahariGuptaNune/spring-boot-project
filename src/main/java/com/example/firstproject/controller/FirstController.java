86a2a3fd2720dd0d3236a29800513b8c3f14c2a7
lahari-cresen <82396441+lahari-cresen@users.noreply.github.com>
2023-09-02 20:43:34 +0530

    added first file

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,23 @@
+package com.example.firstproject.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/first")
+@Slf4j
+public class FirstController {
+
+    @Value("${my-secret-property")
+    private String secretKeyValue;
+
+    @GetMapping(value = "/getMessage")
+    public String getMessage() {
+        log.error("############## Starts:: getMessage ##############");
+        log.error("---- secretKeyValue {} -----", secretKeyValue);
+        return "Hello world";
+    }
+}
