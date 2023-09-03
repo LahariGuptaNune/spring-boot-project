52111ee250b10c5586978dd7141e3b8931a49cea
lahari-cresen <82396441+lahari-cresen@users.noreply.github.com>
2023-09-03 19:03:33 +0530

    update deployment.yml

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -2,6 +2,7 @@ package com.example.firstproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/first")
 @Slf4j
+@PropertySource("classpath:secret.properties")
 public class FirstController {
 
     @Value("${my-secret-property}")
