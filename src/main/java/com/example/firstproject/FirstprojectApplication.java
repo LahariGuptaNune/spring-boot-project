0210774391aa3cce380de5ac15f302335b92b937
LahariGuptaNune <131436331+LahariGuptaNune@users.noreply.github.com>
2023-09-04 10:39:11 +0530

    Update FirstprojectApplication.java

--- src/main/java/com/example/firstproject/FirstprojectApplication.java
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -1,15 +1,27 @@
 package com.example.firstproject;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @PropertySource("classpath:secrets.properties")
+@Slf4j
 public class FirstprojectApplication {
+
+	@Value("${my-secret-property}")
+	private String secretKeyValue;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
 
+	@PostConstruct
+	public void getProp() {
+		log.error("---- secretKeyValue {} -----", secretKeyValue);
+	}
 }
