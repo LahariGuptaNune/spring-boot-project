2efb4f0bf61711df71f99b6ec1109fb50b8f4eb5
lahari-cresen <82396441+lahari-cresen@users.noreply.github.com>
2023-09-04 09:53:45 +0530

    updated file

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -2,7 +2,6 @@ package com.example.firstproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
